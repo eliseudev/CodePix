d3a99b4975081597de6cbff49998ecdf7c6fe11a
Eliseu Oliveira <eliseu.oliveira@credisis.com.br>
2023-10-18 07:25:28 -0300

    Integração Servidor gRPC

---
+++ domain/model/pixKey.go
@@ -0,0 +1,61 @@
+package model
+
+import (
+	"errors"
+	"time"
+
+	"github.com/asaskevich/govalidator"
+	uuid "github.com/satori/go.uuid"
+)
+
+type PixKeyRepositoryInterface interface {
+	RegisterKey(pixKey *PixKey) (*PixKey, error)
+	FindKeyByKind(kind string, pixKey string) (*PixKey, error)
+	AddBank(bank *Bank) error
+	AddAccount(account *Account) error
+	FindAccount(id string) (*Account, error)
+	FindBank(id string) (*Bank, error)
+}
+
+type PixKey struct {
+	Base      `valid:"required"`
+	Kind      string   `json:"kind" gorm:"type:varchar(20)" valid:"notnull"`
+	Key       string   `json:"key" gorm:"type:varchar(255)" valid:"notnull"`
+	AccountID string   `gorm:"column:account_id;type:uuid;not null" valid:"notnull"`
+	Account   *Account `valid:"-"`
+	Status    string   `json:"status" gorm:"type:varchar(20)" valid:"notnull"`
+}
+
+func (pixKey *PixKey) isValid() error {
+	_, err := govalidator.ValidateStruct(pixKey)
+
+	if pixKey.Kind != "email" && pixKey.Kind != "cpf" {
+		return errors.New("invalid type of key")
+	}
+
+	if pixKey.Status != "active" && pixKey.Status != "inactive" {
+		return errors.New("ivalid status")
+	}
+
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func NewPixKey(kind string, account *Account, key string) (*PixKey, error) {
+	pixKey := PixKey{
+		Kind:      kind,
+		Key:       key,
+		Account:   account,
+		AccountID: account.ID,
+		Status:    "active",
+	}
+	pixKey.ID = uuid.NewV4().String()
+	pixKey.CreatedAt = time.Now()
+	err := pixKey.isValid()
+	if err != nil {
+		return nil, err
+	}
+	return &pixKey, nil
+}
